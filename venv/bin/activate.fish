d889081a1ae0f4884560e3ee6db25853b4926bb1
evanwgrenda <evangrenda@gmail.com>
2024-12-20 05:45:45 -0800

    Add Streamlit app files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/evangrenda/Desktop/PostOp_Rag_Streamlit/src/venv"
+set -gx VIRTUAL_ENV "/Users/evangrenda/Desktop/PostOp_Rag_Streamlit_v2/src/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
